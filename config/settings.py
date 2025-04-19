59e21f15ab17dd856589c9866c40a5bda6b291b7
XenaShip <shipxena@gmail.com>
2025-04-19 13:22:14 +0300

    the end.19.04.25

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 
 
 from dotenv import load_dotenv
-from decouple import config
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,7 +84,8 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'db',
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
