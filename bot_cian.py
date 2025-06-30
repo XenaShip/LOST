be55117581d951ae1b06b43241899b5ada3e6df6
XenaShip <shipxena@gmail.com>
2025-06-30 16:17:43 +0300

    30.06.2025

--- bot_cian.py
+++ bot_cian.py
@@ -102,23 +102,24 @@ def escape_md_v2(text):
 
 def fetch_page_data(url):
     """Загружает страницу через undetected_chromedriver и извлекает текст и изображения"""
-    import undetected_chromedriver as uc
+    from webdriver_manager.chrome import ChromeDriverManager
+    from selenium.webdriver.chrome.service import Service
 
     options = uc.ChromeOptions()
-    options.add_argument("--headless=new")  # важно: новый режим
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--disable-features=IsolateOrigins,site-per-process")
-    options.add_argument("--disable-site-isolation-trials")
-    options.add_argument("--remote-debugging-port=9222")
-    options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36")
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36")
 
     driver = None
     try:
+        # Инициализация драйвера с автоматической установкой ChromeDriver
         driver = uc.Chrome(options=options)
+
         driver.set_page_load_timeout(60)
         logging.info(f"Открываю страницу: {url}")
         driver.get(url)
@@ -153,8 +154,6 @@ def fetch_page_data(url):
                 driver.quit()
             except:
                 pass
-
-
 
 
 
